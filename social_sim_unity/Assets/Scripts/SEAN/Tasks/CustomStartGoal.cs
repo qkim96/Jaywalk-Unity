ee217e2a11a22e838f472cb6095fc17e23315bc6
unknown <kkr0913@gmail.com>
2024-12-06 02:20:01 +0900

    Edit custom start goal robot task

--- social_sim_unity/Assets/Scripts/SEAN/Tasks/CustomStartGoal.cs
+++ social_sim_unity/Assets/Scripts/SEAN/Tasks/CustomStartGoal.cs
@@ -10,22 +10,32 @@ namespace SEAN.Tasks
 {
     public class CustomStartGoal : Base
     {
-        Scenario.PedestrianBehavior.LabStudy scenario;
-
-        public GameObject RobotStartLocation;
-        public GameObject RobotGoalLocation;
+        public Vector3 startPosition;
+        public Vector3 goalPosition;
+        public Vector3 startRotation;
+        public Vector3 goalRotation;
+        public Vector3 rotationVector;
 
         protected override bool NewTask()
         {
             robotGoal.SetActive(true);
-            robotStart.transform.position = RobotStartLocation.transform.position;
-            robotStart.transform.rotation = RobotStartLocation.transform.rotation;
-            robotGoal.transform.position = RobotGoalLocation.transform.position;
-            robotGoal.transform.rotation = RobotGoalLocation.transform.rotation;
+
+            startPosition.x = 28.7f;
+            startPosition.y = 0.75f;
+            startPosition.z = -103.6f; //-112.0f;
+            goalPosition.x = 28.7f;
+            goalPosition.y = 0.75f;
+            goalPosition.z = -80.0f;
+            rotationVector = new Vector3(0, 0, 0);
+
+            robotStart.transform.position = startPosition;
+            robotStart.transform.rotation = Quaternion.Euler(rotationVector);
+            robotGoal.transform.position = goalPosition;
+            robotGoal.transform.rotation = Quaternion.Euler(rotationVector);
+            
             SetTargetFlags(robotGoal);
 
             return true;
         }
-
     }
 }
